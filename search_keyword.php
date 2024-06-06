44c34b411e60aa74f7e5adbc0b6cb5fef347de08
Asher Shores <asher.shores5@gmail.com>
2024-06-06 13:34:44 -0700

    favicon working

--- search_keyword.php
+++ search_keyword.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <link rel="icon" type="image/x-icon" href="/img/favicon_io/favicon.ico">
     <style>
         * {
             font-family: Arial, Helvetica, sans-serif;
